f2efcc904d86d2d35f61cff80739d8120b664e57
Fredrick Chibuzor <fchibuzor@getcarbon.co>
2021-12-05 00:16:06 +0100

    blog

---
+++ services/index.js
@@ -0,0 +1,270 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              ... on Author {
+                id
+                name
+                bio
+                photo {
+                  url
+                }
+              }
+            }
+            createdAt
+            excerp
+            slug
+            title
+            featuresImage {
+              url
+            }
+            categories {
+              ... on Category {
+                id
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetGategories {
+        categories {
+          name
+          slug
+        }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+};
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug : String!) {
+      post(where: {slug: $slug}) {
+        title
+        excerp
+        featuresImage {
+          url
+        }
+        author{
+          ... on Author {
+            name
+            bio
+            photo {
+              url
+            }
+          }
+        }
+        createdAt
+        slug
+        content {
+          raw
+        }
+        categories {
+          ... on Category {
+            name
+            slug
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {slug_in: $categories}}
+        last: 3
+      ) {
+        title
+        featuresImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
+
+export const getAdjacentPosts = async (createdAt, slug) => {
+  const query = gql`
+    query GetAdjacentPosts($createdAt: DateTime!,$slug:String!) {
+      next:posts(
+        first: 1
+        orderBy: createdAt_ASC
+        where: {slug_not: $slug, AND: {createdAt_gte: $createdAt}}
+      ) {
+        title
+        featuresImage {
+          url
+        }
+        createdAt
+        slug
+      }
+      previous:posts(
+        first: 1
+        orderBy: createdAt_DESC
+        where: {slug_not: $slug, AND: {createdAt_lte: $createdAt}}
+      ) {
+        title
+        featuresImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug, createdAt });
+
+  return { next: result.next[0], previous: result.previous[0] };
+};
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {slug: $slug}) {
+        edges {
+          cursor
+          node {
+            author {
+              ... on Author {
+                id
+                name
+                bio
+                photo {
+                  url
+                }
+              }
+            }
+            createdAt
+            slug
+            title
+            excerp
+            featuresImage {
+              url
+            }
+            categories {
+              ... on Category {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
+
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetCategoryPost() {
+      posts(where: {featuredPost: true}) {
+        author {
+          ... on Author {
+            name
+            photo {
+              url
+            }
+          }
+        }
+        featuresImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }   
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug:String!) {
+      comments(where: {posts: {slug:$slug}}){
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.comments;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuresImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
