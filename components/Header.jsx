e56a8cdd9d4c602ea97272e39f27c68352912371
Fredrick Chibuzor <fchibuzor@getcarbon.co>
2022-02-14 07:48:55 +0100

    mvp sample

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,6 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
