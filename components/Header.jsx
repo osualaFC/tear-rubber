500bdd7e229fe6f6de3b2b87d42cf89c2c76e9e0
Fredrick Osuala <fredrick.osuala@gmail.com>
2022-04-23 13:41:27 +0100

    tear rubber blog

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            <span className="cursor-pointer font-bold text-5xl text-white">Tear Rubber</span>
             
           </Link>
         </div>
